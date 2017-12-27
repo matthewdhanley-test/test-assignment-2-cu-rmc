0767ced19fa1e602b9b306c4a05a700c7b3ae79c
matt <mattdhanley@gmail.con>
2017-12-27 16:35:57 -0700

    updated some files

--- calibrate.py
+++ calibrate.py
@@ -5,9 +5,33 @@ import math
 import ConfigParser
 from tracker import *
 
+
 def calibrate():
-    #set up the camera feed w/builtin webcam
-    camera = cv2.VideoCapture(0)
+
+    dict = {}
+    tmp = []
+    key = ''
+    filename = -1
+    for arg in sys.argv:
+        if arg[0] == '-':
+            if tmp != []:
+                dict[key] = tmp
+                tmp = []
+            key = arg
+            if key == '-f':
+                dict[key] = sys.argv[sys.argv.index(key)+1]
+                filename = dict[key]
+                print "Using file: "
+                print filename
+                break
+            continue
+        tmp.append(arg)
+    if filename == -1:
+        # set up the camera feed w/builtin webcam
+        print "using builtin webcam..."
+        camera = cv2.VideoCapture(0)
+    else:
+        camera = -1
 
     # make and place a window
     cv2.namedWindow('calibrate')
@@ -37,10 +61,17 @@ def calibrate():
     cv2.createTrackbar('vHigh','calibrate',vh,255,nothing)
     cv2.createTrackbar('area','calibrate',a,100000,nothing)
     cv2.createTrackbar('blur','calibrate',b,30,nothing)
-    #start up stream loop
-    while(1):
-        ret,image = camera.read()
 
+    # start up stream loop
+    while True:
+        if filename == -1:
+            ret, image = camera.read()
+            if not ret:
+                print "Camera not started. Exiting..."
+                return
+        else:
+            image = cv2.imread(filename)
+            image = cv2.resize(image, (0, 0), fx=0.2, fy=0.2)
         # Get values from the endless trackbars
         hLow = cv2.getTrackbarPos('HueLow','calibrate')
         hHigh = cv2.getTrackbarPos('HueHigh','calibrate')
@@ -56,7 +87,7 @@ def calibrate():
 
         blurimg = cv2.GaussianBlur(image,(blur,blur),0)
         cal,mas = mask(blurimg,hLow,hHigh,sLow,sHigh,vLow,vHigh)
-        #threshold the image
+        # threshold the image
         ret, thresh = cv2.threshold(mas,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
 
         save_text = 's - Save settings to config.ini'
@@ -64,21 +95,18 @@ def calibrate():
         cv2.putText(cal,save_text,(0,20),cv2.FONT_HERSHEY_PLAIN,0.9,(0,0,255))
         cv2.putText(cal,quit_text,(0,40),cv2.FONT_HERSHEY_PLAIN,0.9,(0,0,255))
 
-        #find any countours in the thresholded image
+        # find any countours in the thresholded image
         im2,contours,hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL, \
                                                   cv2.CHAIN_APPROX_NONE)
-        found_prev = 0
-        found_area = 0
         if len(contours) != 0:
             # draw in blue the contours that were found
             cv2.drawContours(cal, contours, -1, 255, 3)
 
-            #find the biggest area
+            # find the biggest area
             c = max(contours, key = cv2.contourArea)
             if cv2.contourArea(c) > area:
-                found_prev = found_area
-                found_area = 1
-                #draw a rectangle around the largest contour
+
+                # draw a rectangle around the largest contour
                 x,y,w,h = cv2.boundingRect(c)
                 cv2.putText(image,'Tracking',(x,y-10),cv2.FONT_HERSHEY_PLAIN,\
                             2,(0,0,255),2,cv2.LINE_AA)
@@ -87,22 +115,17 @@ def calibrate():
                 # that we are tracking
                 cx,cy = momentXY(c)
 
-                #plot the center of the countour
+                # plot the center of the countour
                 cv2.circle(image, (cx, cy), 7, (255, 255, 255), -1)
                 # draw the book contour (in green)
                 cv2.rectangle(image,(x,y),(x+w,y+h),(0,255,0),2)
-            else:
-                found_area = 0
-                found_prev = found_area
-
 
         # Show the image
         cv2.imshow("thresh",thresh)
         cv2.imshow("Live feed",image)
         cv2.imshow("calibrate",cal)
 
-
-        #check for input to quit or save
+        # check for input to quit or save
         key = cv2.waitKey(1) & 0xFF
         if key == ord("q"):
             cv2.destroyAllWindows()
@@ -113,7 +136,8 @@ def calibrate():
             updateConfig("Calibrate",hLow,hHigh,sLow,sHigh,vLow,vHigh,area,blur)
             break
 
-    return 1
+    return 0
+
 
 def main():
     calibrate()
--- tracker.py
+++ tracker.py
@@ -1,11 +1,12 @@
 import numpy as np
 import cv2
-import sys
 import math
 import ConfigParser
+import sys
 
 def nothing(x):
     pass
+
 
 def ConfigSectionMap(section):
     # helper function to get values from the config file
@@ -23,6 +24,7 @@ def ConfigSectionMap(section):
             dict1[option] = None
     return dict1
 
+
 def updateConfig(profile,hLow,hHigh,sLow,sHigh,vLow,vHigh,area,blur):
     Config = ConfigParser.RawConfigParser()
     Config.read("config.ini")
@@ -38,8 +40,9 @@ def updateConfig(profile,hLow,hHigh,sLow,sHigh,vLow,vHigh,area,blur):
         Config.write(configfile)
     print "Updated config.ini"
 
+
 def mask(rgbimg,hl,hh,sl,sh,vl,vh):
-    #-------------------------------------------------------------------------------
+    # -------------------------------------------------------------------------------
     # FUNCTION - mask
     # Input Parameters:
     #      rgbimg - red, blue, green channels of an image
@@ -49,39 +52,40 @@ def mask(rgbimg,hl,hh,sl,sh,vl,vh):
     #      sh - high for saturation
     #      vl - low for v
     #      vh - high for v
-    #-------------------------------------------------------------------------------
-    #convert to hsv
-    hsv = cv2.cvtColor(rgbimg,cv2.COLOR_BGR2HSV)
+    # -------------------------------------------------------------------------------
+    # convert to hsv
+    hsv = cv2.cvtColor(rgbimg, cv2.COLOR_BGR2HSV)
 
-    #define lower and upper color limits
-    blueLow = np.array([hl,sl,vl])
-    blueHigh = np.array([hh,sh,vh])
+    # define lower and upper color limits
+    blueLow = np.array([hl, sl, vl])
+    blueHigh = np.array([hh, sh, vh])
 
-    #create a mask by selecting pixels that fall within range
-    mask = cv2.inRange(hsv,blueLow,blueHigh)
+    # create a mask by selecting pixels that fall within range
+    mask = cv2.inRange(hsv, blueLow, blueHigh)
 
-    #use the mask to reveal the image
-    res = cv2.bitwise_and(rgbimg,rgbimg,mask=mask)
+    # use the mask to reveal the image
+    res = cv2.bitwise_and(rgbimg, rgbimg, mask=mask)
 
-    return res,mask
+    return res, mask
+
 
 def momentXY(c):
-    #find center of countour
-    M = cv2.moments(c)
-    cx = int(M['m10']/M['m00'])
-    cy = int(M['m01']/M['m00'])
-    return cx,cy
+    # find center of contour
+    m = cv2.moments(c)
+    cx = int(m['m10']/m['m00'])
+    cy = int(m['m01']/m['m00'])
+    return cx, cy
 
 
-def control(x,y,imgshape):
-    #imgshape - [x,y] shape of the image
+def control(x, y, imgshape):
+    # imgshape - [x,y] shape of the image
     # this will eventually be used to provide control for a motor mounted webcam
     gain = ConfigSectionMap("Controller")['gain']
     middle = imgshape[0]/2
     mag = int(float(middle-x)/middle * gain)
 
-
     return mag
+
 
 def tracking(image,profile):
     # this function takes in an image and a profile from the config file.
@@ -101,7 +105,7 @@ def tracking(image,profile):
     area = settings['area']
     blur = settings['blur']
 
-    #init the centroid
+    # init the centroid
     cx = 0
     cy = 0
 
@@ -111,36 +115,36 @@ def tracking(image,profile):
     # mask to the parameters defined in config.ini
     res,mas = mask(blurimg,hLow,hHigh,sLow,sHigh,vLow,vHigh)
 
-    #threshold the image using otsu (binary vision)
+    # threshold the image using otsu (binary vision)
     ret, thresh = cv2.threshold(mas,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
 
-    #find any countours in the thresholded image
+    # find any countours in the thresholded image
     im2,contours,hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
     if len(contours) != 0:
         # draw in blue the contours that were found
         cv2.drawContours(res, contours, -1, 255, 3)
 
-        #find the biggest area
-        c = max(contours, key = cv2.contourArea)
+        # find the biggest area
+        c = max(contours, key=cv2.contourArea)
 
         # see if contour is large enough to be significant. witout this check,
         # random noise would be enough to trigger the tracker
         if cv2.contourArea(c) > area:
-            #draw a rectangle around the largest contour
-            x,y,w,h = cv2.boundingRect(c)
+            # draw a rectangle around the largest contour
+            x, y, w, h = cv2.boundingRect(c)
 
-            #plot the center of the countour
+            # plot the center of the countour
             cv2.circle(image, (cx, cy), 7, (255, 255, 255), -1)
 
             # draw the book contour (in green)
             cv2.rectangle(image,(x,y),(x+w,y+h),(0,255,0),2)
 
-            #label the rectangle
+            # label the rectangle
             cv2.putText(image,'Tracking',(x,y-10),cv2.FONT_HERSHEY_PLAIN,2,\
                         (0,0,255),2,cv2.LINE_AA)
 
             # get the moment of the blob
-            cx,cy = momentXY(c)
+            cx, cy = momentXY(c)
 
             # controlling left and right camera movement
             # simulate gain with an arrow
@@ -155,11 +159,13 @@ def tracking(image,profile):
 
     return image,cx,cy
 
-def connectCentroid(image,cx1,cx2,cy1,cy2):
-    # draws a line between centroids
-    cv2.line(image, (cx1,cy1), (cx2,cy2), (255,255,0), 2)
 
-def calcLocalize(theta1,theta2,err):
+def connectCentroid(image, cx1, cx2, cy1, cy2):
+    # draws a line between centroids
+    cv2.line(image, (cx1, cy1), (cx2, cy2), (255, 255, 0), 2)
+
+
+def calcLocalize(theta1, theta2, err):
     # this function calculates location based on camera angles
     # not currently used but will be used to localize the camera relative to the
     # blob beacon
@@ -169,11 +175,12 @@ def calcLocalize(theta1,theta2,err):
     x = ConfigSectionMap("PhysicalProps")["cameraseperation"]
     d1 = math.sin(theta2)/math.sin(theta3)*x
     d2 = math.sin(theta1)/math.sin(theta3)*x
-    #---------------------------------------------------------------------------
+    # ---------------------------------------------------------------------------
     # STOPPED HERE 12/2/17-13:00:00
-    #---------------------------------------------------------------------------
+    # ---------------------------------------------------------------------------
 
-def calcAngle(cx1,cx2,cy1,cy2):
+
+def calcAngle(cx1, cx2, cy1, cy2):
     # this function is WIP
     # will be used to calculate the angle of the camera relative to the beacon
     xdist = abs(cx1-cx2)
@@ -187,33 +194,96 @@ def calcAngle(cx1,cx2,cy1,cy2):
     return ratio
 
 
-def main():
+def cameratracking():
     # init the camera
-    camera = cv2.VideoCapture(0)
+    print "Running with webcam..."
+    try:
+        camera = cv2.VideoCapture(0)
+    except:
+        print "Could not set up camera. Exiting..."
+        return
 
     # video loop
-    while(1):
+    while True:
         # get an image
-        ret,image = camera.read()
+        ret, image = camera.read()
+
+        if not ret:
+            print "Failed to read from camera. Exiting..."
+            break
 
         # track colors from config.ini
-        image,cx1,cy1 = tracking(image,"Green2")
-        image,cx2,cy2 = tracking(image,"Pink")
+        image, cx1, cy1 = tracking(image, "Skin")
+        image, cx2, cy2 = tracking(image, "Red")
 
         # if tracking the two colors, connect the centroids and do math
         if cx1 and cx2 and cy1 and cy2:
-            connectCentroid(image,cx1,cx2,cy1,cy2)
-            calcAngle(cx1,cx2,cy1,cy2)
+            connectCentroid(image, cx1, cx2, cy1, cy2)
+            calcAngle(cx1, cx2, cy1, cy2)
 
-        # visualaize the data
-        cv2.imshow("Live",image)
-        
-        #check for input to quit
+        # visualize the data
+        cv2.imshow("Live", image)
+
+        # check for input to quit
         key = cv2.waitKey(1) & 0xFF
         if key == ord("q"):
             cv2.destroyAllWindows()
             break
 
+
+def phototracking(filename):
+    image = cv2.imread(filename)
+    image = cv2.resize(image, (0, 0), fx=0.2, fy=0.2)
+    # track colors from config.ini
+    image, cx1, cy1 = tracking(image, "Yellow")
+    image, cx2, cy2 = tracking(image, "Red")
+    image, cx3, cy3 = tracking(image, "Orange")
+    image, cx4, cy4 = tracking(image, "Green")
+
+
+    # if tracking the two colors, connect the centroids and do math
+    if cx1 and cx2 and cy1 and cy2:
+        connectCentroid(image, cx1, cx2, cy1, cy2)
+        connectCentroid(image, cx2, cx3, cy2, cy3)
+        connectCentroid(image, cx3, cx4, cy3, cy4)
+        connectCentroid(image, cx4, cx1, cy4, cy1)
+
+        calcAngle(cx1, cx2, cy1, cy2)
+
+
+    while True:
+        # visualize the data
+        cv2.imshow(filename, image)
+
+        # check for input to quit
+        key = cv2.waitKey(1) & 0xFF
+        if key == ord("q"):
+            cv2.destroyAllWindows()
+            break
+
+def main():
+    dict = {}
+    tmp = []
+    key = ''
+    filename = -1
+    for arg in sys.argv:
+        if arg[0] == '-':
+            if tmp != []:
+                dict[key] = tmp
+                tmp = []
+            key = arg
+            if key == '-f':
+                dict[key] = sys.argv[sys.argv.index(key)+1]
+                filename = dict[key]
+                print "Using file: "
+                print filename
+                break
+            continue
+        tmp.append(arg)
+    if filename != -1:
+        phototracking(filename)
+    else:
+        cameratracking()
 
 if __name__ == '__main__':
     main()
